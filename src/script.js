f9a5f5a80cc5156872b54f0fe8a4156463849d0b
Kevin <kevin.castro.chavez0@gmail.com>
2023-01-18 15:29:05 -0700

    tested the raycaster on each frame and sphere

--- src/script.js
+++ src/script.js
@@ -40,13 +40,6 @@ scene.add(object1, object2, object3);
 // Raycaster
 
 const raycaster = new THREE.Raycaster();
-const rayOrigin = new THREE.Vector3(-3, 0, 0);
-const rayDirection = new THREE.Vector3(10, 0, 0);
-rayDirection.normalize(); // Becomes a 1 (needs to be normalized)
-
-raycaster.set(rayOrigin, rayDirection);
-
-const intersects = raycaster.intersectObjects([object1, object2, object3]);
 
 /**
  * Sizes
@@ -103,6 +96,28 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.5) * 1.5;
+
+  // Cast a ray
+  const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  const rayDirection = new THREE.Vector3(1, 0, 0);
+  raycaster.set(rayOrigin, rayDirection);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+
+  // Initially colors the spheres
+  for (const object of objectsToTest) {
+    object.material.color.set('#ff0000');
+  }
+
+  // When hit the array, color blue
+  for (const intersect of intersects) {
+    intersect.object.material.color.set('#0000ff');
+  }
 
   // Update controls
   controls.update();
